4ec1fb71fde4a6d3559c9aa15efe4100462b35a2
Matthieu Villeret <matthieuvilleret@yahoo.fr>
2022-02-17 17:39:43 +0100

    can't save new bookmark, rest all done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    raise
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 end
